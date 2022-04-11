1954418cbd29405c959d373869d3e0b71eac0e27
SvetaKirget <kirgetsveta@gmail.com>
2022-04-11 11:55:30 +0300

    finish

--- 18/app18.js
+++ 18/app18.js
@@ -1,3 +1,3 @@
 const a = +prompt("Напишите положительное число");
-console.log(`Сумма цифр = ((''+a).split(''))`);
-// console.log(`Произведение цифр = ${} `);
+console.log(`Сумма цифр = ${String(a).split('').reduce((a,b) => Number (a) + Number (b))}`);
+console.log(`Произведение цифр = ${String(a).split('').reduce((a,b) => Number (a) * Number (b))}`);
