95975d0c7b1cbe8218a48f148b79b5588df5d1eb
SvetaKirget <kirgetsveta@gmail.com>
2022-04-13 12:02:21 +0300

    done

--- 2-12!/app12.js
+++ 2-12!/app12.js
@@ -1,13 +1,29 @@
-const a = +prompt("Введите число 1");
-const b = +prompt("Введите число 2");
-const c = +prompt("Введите число 3");
+let a = +prompt('Число 1');
+let b = +prompt('Число 2');
+let c = +prompt('Число 3');
 
-const valueD = Math.sqrt(b * b - 4 * a * c);
+let value = ((b*b) - (4 * a * c));
 
-if (valueD > 0) {
-  console.log('');
-} else if (valueD = 0) {
-  console.log('');
-} else {
-  console.log('Нет корней');
-}
+console.log(value);
+
+// if (value > 0) {
+// console.log(((-b) + Math.sqrt(value)) / (2 * a) && (((-b) - Math.sqrt(value)) / (2 * a)));
+// } else if (value = 0) {
+//   console.log(-b / (2 * a));
+// } else console.log('Нет корней');
+ 
+
+
+
+
+// const a = +prompt("Введите число 1");
+// const b = +prompt("Введите число 2");
+// const c = +prompt("Введите число 3");
+
+// let valueD = Math.sqrt(b * b - 4 * a * c);
+
+// if (valueD > 0) {
+//   console.log((-b + valueD) / (2 * a) && (-b - valueD) / (2 * a));
+// } else if (valueD = 0) {
+//   console.log(-b / (2 * a));
+// } else console.log("Нет корней");
