1e5bb937edf66a9480df1ec3fbb07768b19e2966
SvetaKirget <kirgetsveta@gmail.com>
2022-04-21 20:14:21 +0300

    all add

--- number_2/2-9/app9.js
+++ number_2/2-9/app9.js
@@ -5,11 +5,10 @@ const b = Math.floor((value % 1000) / 100);
 const c = Math.floor((value % 100) / 10);
 const d = Math.floor(value % 10);
 
-if (!isNaN === value) {
+if (Number(value)) {
+  a % 2 == 0 ? console.log(`${a} - Чётное`) : console.log(`${a} - Нечётное`);
+  b % 2 == 0 ? console.log(`${b} - Чётное`) : console.log(`${b} - Нечётное`);
+  c % 2 == 0 ? console.log(`${c} - Чётное`) : console.log(`${c} - Нечётное`);
+  d % 2 == 0 ? console.log(`${d} - Чётное`) : console.log(`${d} - Нечётное`);
   console.log();
 } else console.log("Некорректный ввод");
-
-a % 2 == 0 ? console.log(`${a} - Чётное`) : console.log(`${a} - Нечётное`);
-b % 2 == 0 ? console.log(`${b} - Чётное`) : console.log(`${b} - Нечётное`);
-c % 2 == 0 ? console.log(`${c} - Чётное`) : console.log(`${c} - Нечётное`);
-d % 2 == 0 ? console.log(`${d} - Чётное`) : console.log(`${d} - Нечётное`);
